b284071e9b9d28d2100c9c6bc8d7c7290babaf0e
unknown <you@example.com>
2021-06-15 01:56:56 +0530

    Moving further with react implementation
      Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /** @format */
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -36,9 +36,16 @@ class App extends Component {
       ],
     };
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
+        <CardList name="pk" />
         {this.state.monsters.map((monsters) => (
           <h1 key={monsters.id}>{monsters.name}</h1>
         ))}
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from "react";
+
+export const CardList = (props) => {
+  console.log(props);
+  return <div>Hello</div>;
+};
